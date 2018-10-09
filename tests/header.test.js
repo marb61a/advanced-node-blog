5cbdf4cdcfd4ec64c57d1dfbea46581b62031128
marb61a <marb61a@gmail.com>
2018-10-09 16:49:31 +0100

    added header tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,3 +10,25 @@ beforeEach(async () => {
 afterEach(async () => {
   await page.close();
 });
+
+test('the header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
+});
