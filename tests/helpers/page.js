c3667b38781b77ae213963d00094162e0c454b8c
marb61a <marb61a@gmail.com>
2018-10-09 11:59:25 +0100

    added initial helper page and setup.js test file

---
+++ tests/helpers/page.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox']
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function(target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+}
