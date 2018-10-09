dbd61331f3b7e023349a29040bcbd2aa20ab1fbe
marb61a <marb61a@gmail.com>
2018-10-09 16:55:05 +0100

    added initial blogs tests

---
+++ tests/blogs.test.js
@@ -0,0 +1,26 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe("When logged in", async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test("can see blog create form", async () => {
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+  });
+  
+});
