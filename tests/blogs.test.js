732b048bf12a4f3852d81ec5e911e8ac2f629f83
marb61a <marb61a@gmail.com>
2018-10-10 11:37:57 +0100

    added tests to blog.test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -30,6 +30,29 @@ describe("When logged in", async () => {
       await page.click('form button');
     });
     
-  })
+    test("Submitting takes user to review screen", async () => {
+      const text = await page.getContentsOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test("Submitting then saving adds blog to index page", async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
+    });
+  });
+
+  describe("And using invalid inputs", async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+  });
 
 });
